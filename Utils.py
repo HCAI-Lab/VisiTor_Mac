b9b3a8ca6a0756ff721346fc4db71bb48d7ad139
ar-zadeh <55898847+ar-zadeh@users.noreply.github.com>
2022-10-21 16:51:55 -0400

    Add files via upload

--- Utils.py
+++ Utils.py
@@ -1,92 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
 
-import sys #Read arguements
-import pyautogui #Finds Patterns, click and keyboard stuff
-from time import sleep # To simulate pause
+# In[ ]:
+
+
+import sys  # Read arguements
+import pyautogui  # Finds Patterns, click and keyboard stuff
+from pathlib import Path  # Contruction of paths
+from time import sleep  # To simulate pause
+import argparse
 import numpy as np  # Matrix stuff
-import os # For moving between folders and stuff
+import os  # For moving between folders and stuff
+
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import shutil #Moving files
-import pickle #Saving files
-from PIL import Image
-from tkinter import *# For GUI i saving files
-from PIL import ImageGrab,ImageTk
-import PIL
-import pygame #Gets mouse position and stuff
-from tkinter import filedialog # GUI stuff
-import glob #finds specific types of files
+import shutil  # Moving files
+import pickle  # Saving files
+# from PIL import Image
+import tkinter  # For GUI i saving files
+# from PIL import ImageGrab
+import pygame  # Gets mouse position and stuff
+from tkinter import filedialog  # GUI stuff
+from tkinter.messagebox import showinfo
+import glob  # finds specific types of files
 import tkinter as tk
 from tkinter import filedialog as fd
+from TemplateMatching import *
 import random
-import win32gui
-import win32con
 from TemplateMatching import *
 
-width = 1920 #self.winfo_screenwidth()
-height = 1080 #self.winfo_screenheight()
 
-#To click through the tkinter
+# Press a key and hold it
 
-def setClickthrough(hwnd):
-    try:
-        styles = win32gui.GetWindowLong(hwnd, win32con.GWL_EXSTYLE)
-        styles = win32con.WS_EX_LAYERED | win32con.WS_EX_TRANSPARENT
-        win32gui.SetWindowLong(hwnd, win32con.GWL_EXSTYLE, styles)
-        win32gui.SetLayeredWindowAttributes(hwnd, 0, 255, win32con.LWA_ALPHA)
-    except Exception as e:
-        print(e)
-root = Tk()
-root.geometry('%dx%d' % (width, height))
-
-
-
-# Create an object of tkinter ImageTk
-img = PIL.Image.open("JSegManEye.jpg")
-resize_image = img.resize((50, 50))
-
-img = ImageTk.PhotoImage(resize_image)
-
-root.title("Applepie")
-root.attributes('-transparentcolor', 'white', '-topmost', 1)
-root.config(bg='white')
-root.attributes("-alpha", 0.5)
-root.wm_attributes("-topmost", 1)
-global bg
-
-bg = Canvas(root, width=width, height=height, bg='white')
-
-# setClickthrough(bg.winfo_id())
-bg = Label(root, image = img)
-
-bg.pack()
-root.overrideredirect(True)
-
-
-
-def create_circle(x, y, r, canvasName):  # center coordinates, radius
-   x0 = x - r
-   y0 = y - r
-   x1 = x + r
-   y1 = y + r
-   return canvasName.create_oval(x0, y0, x1, y1,fill="blue")
-
-
-def naturaleyemove(final_dest,parts = 100):
-
-    final_dest = (int(final_dest[0]),int(final_dest[1]))
-    current = (int(bg.winfo_rootx()),int(bg.winfo_rooty()))
-    for things in getgeomPoints(current,final_dest+(random.uniform(0,10),random.uniform(0,10)),parts):
-        if things == current:
-            continue
-        else:
-            if np.abs(current[0]-int(things[0]))+np.abs(current[1]-int(things[1]))>10:
-                bg.place(x=int(things[0]),y=int(things[1]))
-                root.update()
-                current = things
-                sleep(0.01)
-    bg.place(x=int(things[0]), y=int(things[1]))
-
-def keypress(key, time = 0.1):
+def keypress(key, time=0.1):
     # presses the key
+    print(key[0])
     key = key[0]
     pyautogui.keyDown(key)
     # waits five seconds before releasing the key
@@ -97,122 +44,136 @@ def keypress(key, time = 0.1):
 
 # Click at a specific location of the screen
 def click():
-#If you want mouse down and up to take more time
-    # pyautogui.mouseDown()
-    # sleep(0.1)
-    # pyautogui.mouseUp()
-# If you want just a click:
-    pyautogui.click()
-    root.update()
-# finds the location of a pattern works better than opencv
+    pyautogui.mouseDown()
+    sleep(0.8)
+    pyautogui.mouseUp()
+
+# ==============
+# GUI for finding folder
+def addressfinder():
+    root = tkinter.Tk()
+    root.withdraw()  # use to hide tkinter window
+
+    currdir = os.getcwd()
+    tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
+    return tempdir
+# ==============
+# finds the location of a pattern
 def locate_pic(filename):
-    pic=None
+    pic = None
     while pic is None:
-        root.update_idletasks()
         conf = .8
-        while conf>0.5:
+        while conf > 0.5:
             try:
-                pic = pyautogui.locateOnScreen(filename,confidence=conf)
+                pic = pyautogui.locateOnScreen(filename, confidence=conf)
                 break
             except:
-                conf*=0.9
+                conf *= 0.9
         return pic
-# finding the location of a pattern using opencv and rescaling
+
+
 def locate_pic_CV(filename):
     pic = matching(filename)
     return pic
-#Finds the location of a file in a directory and adds the extention to it
-def find_file(address,filename):
-    for ext in ['png','jpg']:
-        arr = glob.glob(f'{address}/{filename}.{ext}',)
-        if len(arr)>0:
+
+
+# Finds the location of a file in a directory and adds the extention to it
+def find_file(address, filename):
+    for ext in ['png', 'jpg', 'PNG']:
+        arr = glob.glob(f'{address}/{filename}.{ext}', )
+        if len(arr) > 0:
             break
     return arr[0]
-#Finds the location of a pattern
+
+
+# Finds the location of a pattern
 def whereis(path):
     pic = locate_pic(path)
-    if pic ==None:
+    if pic == None:
         return "Pattern doesn't exist"
     else:
-        x = pic[0]+pic[2]/2
-        y = pic[1]+pic[3]/2
-        naturaleyemove((x,y))
-        return x,y
-#Finds the location of the top corner of a pattern
+        x = pic[0] + pic[2] / 2
+        y = pic[1] + pic[3] / 2
+        return x, y
+
+
+# Finds the location of the top corner of a pattern
 def whereis_top(path):
     pic = locate_pic(path)
-    if pic ==None:
+    if pic == None:
         return "Pattern doesn't exist"
     else:
         x = pic[0]
         y = pic[1]
-        return x,y
-#Just clicks
+        return x, y
+
+
+# Just clicks
 def PlainClick():
     pyautogui.click()
-    
-    
-#=========
+
+
+# =========
 # Creates a set of points between an origin and a destination
 def getEquidistantPoints(p1, p2, parts):
-    return zip(np.geomspace(p1[0], p2[0], parts+1),
-               np.geomspace(p1[1], p2[1], parts+1))
-#=========
-#For a more natural movement we need to break the path geometrically
-def getgeomPoints(p1,p2,parts):
-    if (p1[0] ==p2[0]) or (p1[1] == p2[1]):
+    return zip(np.geomspace(p1[0], p2[0], parts + 1),
+               np.geomspace(p1[1], p2[1], parts + 1))
+
+
+# =========
+# A more natural movement
+def getgeomPoints(p1, p2, parts):
+    if (p1[0] == p2[0]) or (p1[1] == p2[1]):
         return [p1]
-    if p2[0]-p1[0]>=0:
-        if p2[1]-p1[1]>=0:
-            return zip([-1*_+p2[0] for _ in list(reversed(np.geomspace(1, p2[0]-p1[0], parts+1)-1))],[_+p2[1] for _ in list(reversed(np.geomspace(1, p2[1]-p1[1], parts+1)-1))])
+    if p2[0] - p1[0] >= 0:
+        if p2[1] - p1[1] >= 0:
+            return zip([-1 * _ + p2[0] for _ in list(reversed(np.geomspace(1, p2[0] - p1[0], parts + 1) - 1))],
+                       [_ + p2[1] for _ in list(reversed(np.geomspace(1, p2[1] - p1[1], parts + 1) - 1))])
         else:
-            return zip([-1*_+p2[0] for _ in list(reversed(np.geomspace(1, p2[0]-p1[0], parts+1)-1))],[_+p2[1] for _ in list(reversed(np.geomspace(1, p1[1]-p2[1], parts+1)-1))])
+            return zip([-1 * _ + p2[0] for _ in list(reversed(np.geomspace(1, p2[0] - p1[0], parts + 1) - 1))],
+                       [_ + p2[1] for _ in list(reversed(np.geomspace(1, p1[1] - p2[1], parts + 1) - 1))])
     else:
-        if p2[1]-p1[1]>=0:
-            return zip([_+p2[0] for _ in list(reversed(np.geomspace(1, p1[0]-p2[0], parts+1)-1))],[-1*_+p2[1] for _ in list(reversed(np.geomspace(1, p2[1]-p1[1], parts+1)-1))])
+        if p2[1] - p1[1] >= 0:
+            return zip([_ + p2[0] for _ in list(reversed(np.geomspace(1, p1[0] - p2[0], parts + 1) - 1))],
+                       [-1 * _ + p2[1] for _ in list(reversed(np.geomspace(1, p2[1] - p1[1], parts + 1) - 1))])
         else:
-            return zip([_+p2[0] for _ in list(reversed(np.geomspace(1, -p2[0]+p1[0], parts+1)-1))],[_+p2[1] for _ in list(reversed(np.geomspace(1, -p2[1]+p1[1], parts+1)-1))])
+            return zip([_ + p2[0] for _ in list(reversed(np.geomspace(1, -p2[0] + p1[0], parts + 1) - 1))],
+                       [_ + p2[1] for _ in list(reversed(np.geomspace(1, -p2[1] + p1[1], parts + 1) - 1))])
 
 
+# =========
 
-#=========
+# =========
 # Will move gradually so it will look like a person
-
-def naturalmove(final_dest,parts = 100):
+# To do: add the argument for type of movement
+def naturalmove(final_dest, parts=100):
     current = pyautogui.position()
-    for things in getgeomPoints(current,final_dest+(random.uniform(0,10),random.uniform(0,10)),parts):
+    for things in getgeomPoints(current, final_dest + (random.uniform(0, 10), random.uniform(0, 10)), parts):
         if current == things:
             continue
-        elif np.abs(current[0]-things[0])+np.abs(current[1]-things[1])>15:
-            pyautogui.moveTo(int(things[0]),int(things[1]))
+        elif np.abs(current[0] - things[0]) + np.abs(current[1] - things[1]) > 15:
+            pyautogui.moveTo(int(things[0]), int(things[1]))
             sleep(0.0000001)
             current = things
     pyautogui.moveTo(int(things[0]) - 5, int(things[1]) - 5)
 
 
-
-    
-#=============
-#Move files from one location to another
+# =============
+# Move files from one location to another
 def movefiles(current, final):
     shutil.move(f"{current}", f"{final}/{current}")
-    
-#==============
-#GUI for finding folder
-def addressfinder():
-    root = Tk()
-    root.withdraw() #use to hide tkinter window
 
-    currdir = os.getcwd()
-    tempdir = filedialog.askdirectory(parent=root, initialdir=currdir, title='Please select a directory')
-    return tempdir
-#=============
 
-#crop stuff
+
+
+
+# =============
+
+# crop stuff
 def displayImage(screen, px, topleft, prior):
     # ensure that the rect always has positive width, height
     x, y = topleft
-    width =  pygame.mouse.get_pos()[0] - topleft[0]
+    width = pygame.mouse.get_pos()[0] - topleft[0]
     height = pygame.mouse.get_pos()[1] - topleft[1]
     if width < 0:
         x += width
@@ -239,36 +200,39 @@ def displayImage(screen, px, topleft, prior):
 
     # return current box extents
     return (x, y, width, height)
+
+
 def setup(path):
     px = pygame.image.load(path)
-    screen = pygame.display.set_mode( px.get_rect()[2:] )
+    screen = pygame.display.set_mode(px.get_rect()[2:])
     screen.blit(px, px.get_rect())
     pygame.display.flip()
     return screen, px
 
+
 def mainLoop(screen, px):
     topleft = bottomright = prior = None
-    n=0
-    while n!=1:
+    n = 0
+    while n != 1:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONUP:
                 if not topleft:
                     topleft = event.pos
                 else:
                     bottomright = event.pos
-                    n=1
+                    n = 1
         if topleft:
             prior = displayImage(screen, px, topleft, prior)
-    return ( topleft + bottomright )
+    return (topleft + bottomright)
 
 
-#================
+# ================
 # User will determine which files to select
 def filefinder(text):
-    #print(text)
+    print(text)
     sleep(1)
-    root = tk.Tk()
-    root.withdraw() #use to hide tkinter window
+    root = tkinter.Tk()
+    root.withdraw()  # use to hide tkinter window
 
     root = tk.Tk()
     root.title('Tkinter Open File Dialog')
@@ -283,19 +247,21 @@ def filefinder(text):
         initialdir='/',
         filetypes=filetypes)
     return filenames
-#=================
+
+
+# =================
 # User selects where they have saved their files
 def retreaveinfo():
     print('please show where you have saved the files')
     sleep(1)
     directory = addressfinder()
     os.chdir(directory)
-    pickels = glob.glob(f"{directory}\*.pkl", recursive = True)
+    pickels = glob.glob(f"{directory}\*.pkl", recursive=True)
     if f'{directory}\\choices.pkl' in pickels:
         open_file = open(f'{directory}\\choices.pkl', "rb")
         choices = pickle.load(open_file)
         open_file.close()
-        choices = [f'{directory}\\{_}'for _ in choices]
+        choices = [f'{directory}\\{_}' for _ in choices]
     else:
         choices_address = filefinder('please choose your choices and win lose setuations')
     if f'{directory}\\coordinates.pkl' in pickels:
@@ -306,12 +272,31 @@ def retreaveinfo():
         open_file = open(coordinates_address, "rb")
         coordinates = pickle.load(open_file)
         open_file.close()
-    types = ('*.png', '*.jpg') # the tuple of file types
+    types = ('*.png', '*.jpg')  # the tuple of file types
     pictures = []
     for files in types:
-        pictures.extend(glob.glob(f'{directory}/{files}.png', recursive = True))
+        pictures.extend(glob.glob(f'{directory}/{files}.png', recursive=True))
     pictures = [_.split('.')[0] for _ in pictures]
     if f'{directory}\\environment' not in pictures:
         environment = filefinder('please choose your environment file')
     return choices
-#===============
+
+
+
+# ===============
+def play_game(action):
+    coor_choice = whereis(f'{action}.png')
+    naturalmove((int(coor_choice[0]), int(coor_choice[1])))
+    click()
+    win = whereis(f'win.png')
+    lose = (whereis(f'lose.png'))
+    if type(whereis(f'win.png')) == tuple:
+        print(f'Match')
+        results = 1
+    elif type(whereis(f'lose.png')) == tuple:
+        print(f'Wrong')
+        results = -1
+    else:
+        results = play_game(action)
+    return results
+
